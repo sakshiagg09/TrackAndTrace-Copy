9990a954113766419606d42ea25938174390285a
sakshiagg09 <sakshi.aggarwal@nav-it.com>
2026-01-14 16:26:18 +0530

    Backend Folder Deleted

--- backend/services/tmParser.service.js
+++
@@ -1,9 +0,0 @@
-export function parseFinalInfo(finalInfo) {
-  try {
-    if (!finalInfo || finalInfo === "[]") return [];
-    return JSON.parse(finalInfo);
-  } catch (err) {
-    console.error("FinalInfo parse error:", err);
-    return [];
-  }
-}
